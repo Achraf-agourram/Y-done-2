10e6a1f81d272a041e05e67e3c36cb6c65a0b3bd
Achraf-podcast <achrafagourram6@gmail.com>
2026-02-09 17:03:42 +0100

    added Day relations

--- laravel/app/Models/Day.php
+++ laravel/app/Models/Day.php
@@ -2,9 +2,21 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Day extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'day',
+        'opening',
+        'closing',
+    ];
+
+    public function schedule()
+    {
+        return $this->belongsTo(Schedule::class);
+    }
 }
