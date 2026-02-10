a73cb241bf921a50740dcd03cc42f2a43df687af
Achraf-podcast <achrafagourram6@gmail.com>
2026-02-10 12:30:04 +0100

    added dayFactory

---
+++ laravel/database/factories/DayFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Day>
+ */
+class DayFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'day' => $this->faker->dayOfWeek,
+            'opening' => '09:00',
+            'closing' => '23:00',
+            'schedule_id' => null,
+        ];
+    }
+}
