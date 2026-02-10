a869b49280999176124511d5f1e55b46924e2e90
Achraf-podcast <achrafagourram6@gmail.com>
2026-02-10 11:14:22 +0100

    added booking with restaurant relation

--- laravel/app/Models/Booking.php
+++ laravel/app/Models/Booking.php
@@ -2,9 +2,22 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Booking extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'bookingDay',
+        'tables',
+        'startHour',
+        'endHour',
+    ];
+
+    public function restaurant()
+    {
+        return $this->belongsTo(Restaurent::class);
+    }
 }
--- laravel/app/Models/Restaurent.php
+++ laravel/app/Models/Restaurent.php
@@ -50,6 +50,6 @@ class Restaurent extends Model
 
     public function bookings()
     {
-        return $this->belongsToMany(Booking::class);
+        return $this->hasMany(Booking::class);
     }
 }
