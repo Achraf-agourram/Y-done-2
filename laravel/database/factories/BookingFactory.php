08cd28a0bee25c39279b3f7c43d54e6ae0d21019
Achraf-podcast <achrafagourram6@gmail.com>
2026-02-10 12:34:10 +0100

    added bookingFactory

---
+++ laravel/database/factories/BookingFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Booking>
+ */
+class BookingFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        $start = $this->faker->numberBetween(12, 20);
+
+        return [
+            'bookingDay' => $this->faker->date(),
+            'tables' => $this->faker->numberBetween(1, 2),
+            'startHour' => sprintf('%02d:00', $start),
+            'endHour' => sprintf('%02d:00', $start + 2),
+            'restaurent_id' => null,
+        ];
+    }
+}
