fad97862f1a2038abc4c525beb8956f91c1ff121
Achraf-podcast <achrafagourram6@gmail.com>
2026-02-10 12:08:43 +0100

    added bookings migration with foreign keys

---
+++ laravel/database/migrations/2026_02_10_104731_create_bookings_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('bookings', function (Blueprint $table) {
+            $table->id();
+            $table->date('bookingDay');
+            $table->integer('tables');
+            $table->time('startHour');
+            $table->time('endHour');
+            $table->timestamps();
+
+            $table->foreignId('restaurent_id')->constrained()->cascadeOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('bookings');
+    }
+};
